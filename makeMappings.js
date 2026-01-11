8f88b1da4df7cffbb28dc71a815d1b7c6cb2881a
WaveDev <wavedev@github.com>
2026-01-11 21:59:44 +0100

    fix more shit and add constants

--- makeMappings.js
+++ makeMappings.js
@@ -28,22 +28,25 @@ import fs from 'fs/promises';
             }
         })();
     });
+    const constants = JSON.parse(
+        await fs.readFile('./data/constants.json', 'utf-8'),
+    );
     const data = JSON.parse(await fs.readFile('./data/data.json', 'utf-8'));
     const result = await page.evaluate(
-        (data) => {
+        (data, constants) => {
             let _mods = Object.values(r.c);
-            const chunks = Object.entries(wreq.m);
+            const chunks = Object.entries(r.m);
             const findChunkByCode = (...codes) => {
                 for (let i = 0; i < chunks.length; i++) {
                     const [id, func] = chunks[i];
                     const chunkCode = func.toString();
 
                     if (codes.every((code) => chunkCode.includes(code)))
-                        return [id, wreq(id)];
+                        return [id, r(id)];
                 }
             };
             const httpUtils = findChunkByCode('HTTPUtils');
-            const codeH = wreq.m[httpUtils[0]].toString();
+            const codeH = r.m[httpUtils[0]].toString();
             const decodeHttpUtils = (module) => {
                 const mappings = {};
                 const requestHandler = codeH.match(
@@ -118,6 +121,40 @@ import fs from 'fs/promises';
                 id: httpUtils[0],
                 path: '../discord_common/js/packages/http-utils/HTTPUtils.tsx',
                 mappings: decodeHttpUtils(httpUtils[1]),
+            });
+            const constantsModule = findChunkByCode('/users/@me');
+
+            const testAll = (val, finders) => {
+                for (let finder of finders) {
+                    if (finder.type === 'toStringIncludes')
+                        if (val.toString().includes(finder.value))
+                            return finder;
+                    if (finder.type === 'ByProps')
+                        if (finder.value.every((prop) => val?.[prop]))
+                            return finder;
+                    if (finder.type === 'eq' && finder.value === val)
+                        return finder;
+                    if (
+                        finder.type === 'includes' &&
+                        finder.value?.includes?.(val)
+                    )
+                        return finder;
+                }
+            };
+            const map = (exports, finders) => {
+                const mappings = {};
+                for (let key in exports) {
+                    const val = exports;
+                    const match = testAll(val, finders);
+                    if (match) mappings[key] = match.name;
+                }
+                return mappings;
+            };
+
+            output.push({
+                id: constantsModule[0],
+                path: '../discord_common/js/shared/Constants.tsx',
+                mappings: map(constantsModule[1], constants),
             });
             return output;
         },
